fc93cf87824ab65c1c58259f958ac109156327ec
FrancoYudica <francoyudica@gmail.com>
2026-05-18 13:42:15 -0300

    Fixed all project warnings

--- restaurant-server/src/main/java/com/cm/restaurant_server/controllers/ContactoController.java
+++ restaurant-server/src/main/java/com/cm/restaurant_server/controllers/ContactoController.java
@@ -1,18 +1,35 @@
 package com.cm.restaurant_server.controllers;
 
-import com.cm.restaurant_server.business.domain.dto.contacto.ContactoCreateDto;
 import com.cm.restaurant_server.business.domain.dto.contacto.ContactoDto;
 import com.cm.restaurant_server.business.domain.entity.Contacto;
 import com.cm.restaurant_server.business.logic.service.ContactoService;
 import com.cm.restaurant_server.business.mapper.ContactoMapper;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/contactos")
-public class ContactoController extends BaseController<Contacto, ContactoDto, ContactoCreateDto, ContactoCreateDto> {
+public class ContactoController {
+
+    private final ContactoService service;
+    private final ContactoMapper mapper;
 
     public ContactoController(ContactoService service, ContactoMapper mapper) {
-        super(service, mapper);
+        this.service = service;
+        this.mapper = mapper;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ContactoDto>> getAll() throws Exception {
+        List<Contacto> entities = service.findAll();
+        return ResponseEntity.ok(mapper.toDTOsList(entities));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ContactoDto> getById(@PathVariable String id) throws Exception {
+        Contacto entity = service.findById(id);
+        return ResponseEntity.ok(mapper.toDTO(entity));
     }
 }
